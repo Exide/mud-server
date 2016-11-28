758d8d7232c16ce0ed0ee93e6a8668cee235e6e7
exide <exide@hotmail.com>
2016-11-27 20:08:35 -0800

    Pulled the telnet port from the config file.

--- src/main/java/org/arabellan/mud/Server.java
+++ src/main/java/org/arabellan/mud/Server.java
@@ -7,28 +7,49 @@ import org.arabellan.mud.network.SocketProcessor;
 import org.arabellan.mud.network.SocketListener;
 import org.arabellan.mud.network.Socket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Slf4j
 public class Server {
 
-    private static final int TELNET_PORT = 23;
-
     public static void main(String[] args) {
         new Server().run();
     }
 
     private void run() {
+        Properties config = loadConfigFile("/config.properties");
+        validateConfig(config);
+
         Queue<Socket> socketQueue = new ConcurrentLinkedQueue<>();
         EventBus eventBus = new EventBus();
 
-        Thread telnetListenerThread = new Thread(new SocketListener(TELNET_PORT, socketQueue));
+        int telnetPort = Integer.valueOf(config.getProperty("telnetPort"));
+        Thread telnetListenerThread = new Thread(new SocketListener(telnetPort, socketQueue));
         Thread socketProcessingThread = new Thread(new SocketProcessor(socketQueue, eventBus));
         Thread messageProcessingThread = new Thread(new MessageProcessor(eventBus));
 
         messageProcessingThread.start();
         socketProcessingThread.start();
         telnetListenerThread.start();
+    }
+
+    private Properties loadConfigFile(String filename) {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = Server.class.getResourceAsStream(filename);
+            properties.load(inputStream);
+            return properties;
+        } catch (IOException e) {
+            throw new RuntimeException("cannot load config file: " + filename, e);
+        }
+    }
+
+    private void validateConfig(Properties config) {
+        if (config.get("telnetPort") == null)
+            throw new RuntimeException("missing \"telnetPort\" setting");
     }
 }
