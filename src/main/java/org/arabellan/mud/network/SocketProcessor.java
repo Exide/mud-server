ec0a29a9bc67fc06372b86d144dbcc2cf492a82d
exide <exide@hotmail.com>
2016-11-27 18:08:56 -0800

    Non-option telnet commands are now ignored instead of crashing the server.

--- src/main/java/org/arabellan/mud/network/SocketProcessor.java
+++ src/main/java/org/arabellan/mud/network/SocketProcessor.java
@@ -15,8 +15,35 @@ import java.util.Set;
 
 import static org.arabellan.mud.network.Socket.State.CLOSED;
 
+/**
+ * Handle non-blocking socket reads and writes using the Telnet specification
+ *
+ * @see <a href="https://tools.ietf.org/html/rfc854">RFC 854: Telnet Protocol Specification</a>
+ * @see <a href="https://tools.ietf.org/html/rfc855">RFC 855: Telnet Option Specification</a>
+ */
+
 @Slf4j
 public class SocketProcessor implements Runnable {
+
+    private static final byte LINE_FEED = (byte) 10;
+    private static final byte CARRIAGE_RETURN = (byte) 13;
+
+    private static final byte SUBNEGOTIATION_END = (byte) 240;
+    private static final byte NO_OPERATION = (byte) 241;
+    private static final byte DATA_MARK = (byte) 242;
+    private static final byte BREAK = (byte) 243;
+    private static final byte INTERRUPT_PROCESS = (byte) 244;
+    private static final byte ABORT_OUTPUT = (byte) 245;
+    private static final byte ARE_YOU_THERE = (byte) 246;
+    private static final byte ERASE_CHARACTER = (byte) 247;
+    private static final byte ERASE_LINE = (byte) 248;
+    private static final byte GO_AHEAD = (byte) 249;
+    private static final byte SUBNEGOTIATION_BEGIN = (byte) 250;
+    private static final byte WILL = (byte) 251;
+    private static final byte WONT = (byte) 252;
+    private static final byte DO = (byte) 253;
+    private static final byte DONT = (byte) 254;
+    private static final byte INTERPRET_AS_COMMAND = (byte) 255;
 
     private final Queue<Socket> socketQueue;
     private final Map<Integer, Socket> socketMap;
@@ -77,8 +104,6 @@ public class SocketProcessor implements Runnable {
                     Socket socket = (Socket) key.attachment();
                     ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-                    log.debug("Socket " + socket.getId() + " read selected");
-
                     int bytesRead = socket.getSocketChannel().read(buffer);
                     while (bytesRead > 0) {
                         bytesRead = socket.getSocketChannel().read(buffer);
@@ -92,25 +117,28 @@ public class SocketProcessor implements Runnable {
 
                     buffer.flip();
 
+                    byte[] bytes = new byte[buffer.limit()];
+                    buffer.get(bytes);
+                    buffer.rewind();
+                    String input = new String(bytes);
+                    String trimmedInput = input.trim();
+                    log.debug("From " + socket.getId() + ": " + trimmedInput);
+
                     while (buffer.hasRemaining()) {
-                        if (TelnetProtocol.nextByteIAC(buffer)) {
-                            ByteBuffer request = TelnetProtocol.extractTelnetCommand(buffer);
-                            ByteBuffer response = TelnetProtocol.parseTelnetCommand(request);
-                            socket.addWriteSelector(writeSelector);
+                        if (nextByteIAC(buffer)) {
+                            ByteBuffer response = handleTelnetCommand(buffer);
                             socket.getOutgoingQueue().add(response);
                         } else {
-                            byte[] bytes = new byte[buffer.limit()];
-                            buffer.get(bytes);
-                            String output = new String(bytes);
-                            String trimmedOutput = output.trim();
-                            log.debug("From " + socket.getId() + ": " + trimmedOutput);
-                            trimmedOutput += "\r\n";
-                            ByteBuffer outputBuffer = ByteBuffer.allocate(trimmedOutput.length());
-                            outputBuffer.put(trimmedOutput.getBytes());
-                            outputBuffer.flip();
-                            IncomingMessageEvent message = new IncomingMessageEvent(socket.getId(), outputBuffer);
+                            ByteBuffer clonedBuffer = ByteBuffer.allocate(buffer.remaining());
+                            clonedBuffer.put(buffer);
+                            clonedBuffer.flip();
+                            IncomingMessageEvent message = new IncomingMessageEvent(socket.getId(), clonedBuffer);
                             eventBus.post(message);
                         }
+                    }
+
+                    if (socket.getOutgoingQueue().size() > 0) {
+                        socket.addWriteSelector(writeSelector);
                     }
                 }
 
@@ -129,15 +157,18 @@ public class SocketProcessor implements Runnable {
 
                 for (SelectionKey key : keys) {
                     Socket socket = (Socket) key.attachment();
-                    ByteBuffer message = socket.getOutgoingQueue().poll();
+                    ByteBuffer buffer = socket.getOutgoingQueue().poll();
 
-                    if (message != null && message.hasRemaining()) {
+                    if (buffer != null && buffer.hasRemaining()) {
 
-                        if (socket.getProtocol() == Socket.Protocol.TELNET && !TelnetProtocol.nextByteIAC(message)) {
-                            log.debug("To " + socket.getId() + ": " + StringUtils.fromByteBuffer(message));
-                        }
+                        byte[] bytes = new byte[buffer.limit()];
+                        buffer.get(bytes);
+                        buffer.rewind();
+                        String output = new String(bytes);
+                        String trimmedOutput = output.trim();
+                        log.debug("To " + socket.getId() + ": " + trimmedOutput);
 
-                        socket.getSocketChannel().write(message);
+                        socket.getSocketChannel().write(buffer);
                     }
 
                     if (socket.getOutgoingQueue().size() == 0) {
@@ -152,13 +183,74 @@ public class SocketProcessor implements Runnable {
         }
     }
 
+    private boolean nextByteIAC(ByteBuffer buffer) {
+        if (buffer.remaining() == 0)
+            throw new IllegalArgumentException("buffer is empty");
+
+        buffer.mark();
+        byte b = buffer.get();
+        buffer.reset();
+        return b == INTERPRET_AS_COMMAND;
+    }
+
+    private ByteBuffer handleTelnetCommand(ByteBuffer buffer) {
+        if (buffer.remaining() == 0)
+            throw new IllegalArgumentException("buffer is empty");
+
+        if (buffer.remaining() == 1)
+            throw new IllegalArgumentException("buffer doesn't have enough bytes");
+
+        byte iac = buffer.get();
+        byte command = buffer.get();
+
+        ByteBuffer response;
+
+        if (isOptionRequest(command)) {
+            byte option = buffer.get();
+            log.debug("Ignoring telnet command: " + convertByteToInt(command) + " " + convertByteToInt(option));
+            response = handleOptionRequest(command, option);
+        } else {
+            log.debug("Ignoring telnet command: " + convertByteToInt(command));
+            response = ByteBuffer.allocate(2);
+            response.put(CARRIAGE_RETURN);
+            response.put(LINE_FEED);
+        }
+
+        return response;
+    }
+
+    private boolean isOptionRequest(byte command) {
+        return command == WILL || command == WONT || command == DO || command == DONT;
+    }
+
+    private ByteBuffer handleOptionRequest(byte command, byte option) {
+        ByteBuffer output = ByteBuffer.allocate(3);
+        output.put(INTERPRET_AS_COMMAND);
+
+        if (command == DO) output.put(WONT);
+        if (command == WILL) output.put(DONT);
+
+        output.put(option);
+        output.flip();
+
+        return output;
+    }
+
+    private int convertByteToInt(byte b) {
+        return b & 0xFF;
+    }
+
+    private byte convertIntToByte(int i) {
+        return (byte) i;
+    }
+
     private class OutgoingMessageHandler {
         @Subscribe
         public void handle(OutgoingMessageEvent event) {
             log.trace("Handling OutgoingMessageEvent");
             Socket socket = socketMap.get(event.getId());
-            socket.addWriteSelector(writeSelector);
             socket.getOutgoingQueue().add(event.getBuffer());
+            socket.addWriteSelector(writeSelector);
         }
     }
 
@@ -167,8 +259,12 @@ public class SocketProcessor implements Runnable {
         public void handle(BroadcastMessageEvent event) {
             log.trace("Handling BroadcastMessageEvent");
             for (Socket socket : socketMap.values()) {
+                ByteBuffer clone = ByteBuffer.allocate(event.getBuffer().remaining());
+                clone.put(event.getBuffer());
+                event.getBuffer().rewind();
+                clone.flip();
+                socket.getOutgoingQueue().add(clone);
                 socket.addWriteSelector(writeSelector);
-                socket.getOutgoingQueue().add(event.getBuffer());
             }
         }
     }
